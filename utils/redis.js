3e490022cf6505b54024bbd38933d42e6a7933d0
simonrichard-dev <simon.insa@gmail.com>
2024-04-03 19:00:12 +0200

    add files for task 0. Redis utils and yml docker file

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+const redis = require('redis');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.client.on('error', (error) => {
+            console.error('Redis client error:', error);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (error, reply) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (error, reply) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (error, reply) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
